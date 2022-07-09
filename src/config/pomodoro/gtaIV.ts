1c2ff012e716d856093d1642fd14f7059751a7e6
Rodrigo Fernandes <me@rodrigofernand.es>
2022-07-09 20:21:53 -0300

    refactor audio

--- src/config/pomodoro/gtaIV.ts
+++ src/config/pomodoro/gtaIV.ts
@@ -1,26 +1,26 @@
-import start from 'assets/audio/gta_iv/GTA_IV-Start.mp3'
+const start = '/audio/gta_iv/GTA_IV-Start.mp3'
 
-import stop from 'assets/audio/gta_iv/GTA_IV-Stop.mp3'
+const stop = '/audio/gta_iv/GTA_IV-Stop.mp3'
 
-import finish1 from 'assets/audio/gta_iv/GTA_IV-Finish_1.mp3'
-import finish2 from 'assets/audio/gta_iv/GTA_IV-Finish_2.mp3'
-import finish3 from 'assets/audio/gta_iv/GTA_IV-Finish_3.mp3'
-import finish4 from 'assets/audio/gta_iv/GTA_IV-Finish_4.mp3'
-import finish5 from 'assets/audio/gta_iv/GTA_IV-Finish_5.mp3'
-import finish6 from 'assets/audio/gta_iv/GTA_IV-Finish_6.mp3'
-import finish7 from 'assets/audio/gta_iv/GTA_IV-Finish_7.mp3'
-import finish8 from 'assets/audio/gta_iv/GTA_IV-Finish_8.mp3'
-import finish9 from 'assets/audio/gta_iv/GTA_IV-Finish_9.mp3'
+const finish1 = '/audio/gta_iv/GTA_IV-Finish_1.mp3'
+const finish2 = '/audio/gta_iv/GTA_IV-Finish_2.mp3'
+const finish3 = '/audio/gta_iv/GTA_IV-Finish_3.mp3'
+const finish4 = '/audio/gta_iv/GTA_IV-Finish_4.mp3'
+const finish5 = '/audio/gta_iv/GTA_IV-Finish_5.mp3'
+const finish6 = '/audio/gta_iv/GTA_IV-Finish_6.mp3'
+const finish7 = '/audio/gta_iv/GTA_IV-Finish_7.mp3'
+const finish8 = '/audio/gta_iv/GTA_IV-Finish_8.mp3'
+const finish9 = '/audio/gta_iv/GTA_IV-Finish_9.mp3'
 
-import resetSoundFile from 'assets/audio/gta_iv/GTA_IV-Reset.mp3'
+const resetSoundFile = '/audio/gta_iv/GTA_IV-Reset.mp3'
 
-import specialFinish1 from 'assets/audio/gta_iv/GTA_IV-Special_Finish_1.mp3'
-import specialFinish2 from 'assets/audio/gta_iv/GTA_IV-Special_Finish_2.mp3'
-import specialFinish3 from 'assets/audio/gta_iv/GTA_IV-Special_Finish_3.mp3'
-import specialFinish4 from 'assets/audio/gta_iv/GTA_IV-Special_Finish_4.mp3'
-import specialFinish5 from 'assets/audio/gta_iv/GTA_IV-Special_Finish_5.mp3'
+const specialFinish1 = '/audio/gta_iv/GTA_IV-Special_Finish_1.mp3'
+const specialFinish2 = '/audio/gta_iv/GTA_IV-Special_Finish_2.mp3'
+const specialFinish3 = '/audio/gta_iv/GTA_IV-Special_Finish_3.mp3'
+const specialFinish4 = '/audio/gta_iv/GTA_IV-Special_Finish_4.mp3'
+const specialFinish5 = '/audio/gta_iv/GTA_IV-Special_Finish_5.mp3'
 
-import finishBreak from 'assets/audio/gta_iv/GTA_IV-Finish_Break.mp3'
+const finishBreak = '/audio/gta_iv/GTA_IV-Finish_Break.mp3'
 
 export const startSound = start
 
