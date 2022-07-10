43ef1a92c0da76d072ea1c94b99e5a995680a32e
Rodrigo Fernandes <me@rodrigofernand.es>
2022-07-09 21:18:41 -0300

    icons resizing

--- src/components/Icons/Send.tsx
+++ src/components/Icons/Send.tsx
@@ -5,8 +5,8 @@ import { SVGProps } from './types'
 export const Send = ({ title, titleId, ...props }: SVGProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
+    width="100%"
+    height="100%"
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
